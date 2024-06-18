6b06132a9b33062d088baea5a47928f45d856d0d
Yash Varma <71536409+yash509@users.noreply.github.com>
2024-06-18 09:58:01 +0530

    Create Grafana.sh

---
+++ Shell Scripts/Grafana.sh
@@ -0,0 +1,21 @@
+# File for Grafana Installation
+
+
+
+sudo apt-get update
+
+sudo apt-get install -y apt-transport-https software-properties-common
+
+wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
+
+echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
+
+sudo apt-get update
+
+sudo apt-get -y install grafana
+
+sudo systemctl enable grafana-server
+
+sudo systemctl start grafana-server
+
+sudo systemctl status grafana-server
