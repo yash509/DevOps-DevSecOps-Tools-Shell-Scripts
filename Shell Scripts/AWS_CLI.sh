028f8aa7fca694ede79f515cdb510c5bf5805ec9
Yash Varma <71536409+yash509@users.noreply.github.com>
2024-06-18 09:54:54 +0530

    Create AWS_CLI.sh

---
+++ Shell Scripts/AWS_CLI.sh
@@ -0,0 +1,11 @@
+# File for AWS-CLI Installation
+
+
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+
+sudo apt-get install unzip -y
+
+unzip awscliv2.zip
+
+sudo ./aws/install
